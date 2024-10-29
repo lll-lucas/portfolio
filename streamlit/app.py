5a1a62886e0e2e8bf565c2d9f2a756f12c31a02d
Lucas Barboza <lllucas.barboza@gmail.com>
2024-10-29 16:56:20 -0300

    Update app.py

--- streamlit/app.py
+++ streamlit/app.py
@@ -52,6 +52,26 @@ st.markdown(
     unsafe_allow_html=True
 )
 
+# Estilo CSS para ícones horizontal no desktop e rolagem no celular
+st.markdown(
+    """
+    <style>
+    .horizontal-icons {
+        display: flex;
+        justify-content: center;
+        flex-wrap: wrap;
+        gap: 10px;
+    }
+    .horizontal-icons img {
+        width: 48px;
+        height: auto;
+        display: inline-block;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
 st.write("##")
 st.markdown("<h3 style='text-align: center; text-decoration: none;'>Olá, visitante! 👋</h3>", unsafe_allow_html=True)
 st.markdown("<h1 style='text-align: center; text-decoration: none;'>Meu nome é Lucas</h1>", unsafe_allow_html=True)
@@ -89,19 +109,15 @@ if selected == 'About':
                      a extração de insights valiosos, fundamentais para orientação na tomada de decisões estrategicas. </div>""", unsafe_allow_html=True)
             st.markdown(""" <div style='text-align: justify; font-size: 1.2em;'> Entre as principais tecnologias que domino estão: </div>""", unsafe_allow_html=True)
             st.write("") 
-            col3, col4, col5, col6, col7, col8 = st.columns(6) 
-            with col3: 
-                st.image(os.path.join(current_dir, "python.png"), width=48) 
-            with col4: 
-                st.image(os.path.join(current_dir, "r.png"), width=48) 
-            with col5: 
-                st.image(os.path.join(current_dir, "mysql.png"), width=48) 
-            with col6: 
-                st.image(os.path.join(current_dir, "postgresql.png"), width=48) 
-            with col7: 
-                st.image(os.path.join(current_dir, "power_bi.png"), width=48) 
-            with col8: 
-                st.image(os.path.join(current_dir, "excel.png"), width=48)
+            # Div para ícones horizontalmente ajustáveis
+            st.markdown("<div class='horizontal-icons'>", unsafe_allow_html=True)
+            st.image(os.path.join(current_dir, "python.png"), width=48)
+            st.image(os.path.join(current_dir, "r.png"), width=48)
+            st.image(os.path.join(current_dir, "mysql.png"), width=48)
+            st.image(os.path.join(current_dir, "postgresql.png"), width=48)
+            st.image(os.path.join(current_dir, "power_bi.png"), width=48)
+            st.image(os.path.join(current_dir, "excel.png"), width=48)
+            st.markdown("</div>", unsafe_allow_html=True)
             st.write("") 
             st.markdown(""" <div style='text-align: justify; font-size: 1.2em;'> Ficou interessado? Acesse a sessão de projetos para ver alguns dos meus principais trabalhos. Lá, você 
                         encontrará estudos de caso detalhados, análises aprofundadas e soluções inovadoras que desenvolvi ao longo da minha trajetória. Não perca a oportunidade de conhecer
